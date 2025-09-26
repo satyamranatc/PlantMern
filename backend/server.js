dc85433dc143c9932d9e4280135ddcedaf75f865
Satyam Rana <satyamranatc@gmail.com>
2025-09-26 17:32:23 +0530

    Env Updated

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,10 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
+  origin:[
+    "https://plantmernfrontend.onrender.com/",
+    "http://localhost:5173"
+  ]
 }));
 app.use(express.json());
 
